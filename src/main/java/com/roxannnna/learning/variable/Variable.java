57426c1f485075582a1336bbfc883351086fb94a
Roxannnna <roksana2107@gmail.com>
2023-01-29 13:39:01 +0100

    learning

---
+++ src/main/java/com/roxannnna/learning/variable/Variable.java
@@ -0,0 +1,22 @@
+package com.roxannnna.learning.variable;
+
+//https://docs.oracle.com/javase/tutorial/java/nutsandbolts/if.html
+public class Variable {
+
+    public static void main(String[] args) {
+
+        char someChar = 'd';
+
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        int w = y - 2;
+        z = x + y;
+
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+        System.out.println("Z: " + z);
+
+        System.out.println("Finished");
+    }
+}
