1277801141eac46eb98c08d34bca519f7c0ac3e1
Roxannnna <roksana2107@gmail.com>
2023-01-31 18:21:58 +0100

    learning

---
+++ src/main/java/com/roxannnna/learning/variable/Array.java
@@ -0,0 +1,19 @@
+package com.roxannnna.learning.variable;
+
+public class Array {
+    public static void main(String [] args) {
+        int[] anArray;
+        anArray = new int [5];
+        anArray[0] = 7;
+        anArray[1] = 90;
+        anArray[2] = 189;
+        anArray[3] = 556;
+        anArray[4] = 994;
+
+        System.out.println("Element at index 0: " + anArray[0]);
+        System.out.println("Element at index 1: " + anArray[1]);
+        System.out.println("Element at index 2: " + anArray[2]);
+        System.out.println("Element at index 3: " + anArray[3]);
+        System.out.println("Element at index 4: " + anArray[4]);
+    }
+}
