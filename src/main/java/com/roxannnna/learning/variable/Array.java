b0e44c64abea386fba8563b12f8b29564184bdcf
Roxannnna <roksana2107@gmail.com>
2023-01-31 18:31:31 +0100

    learning

--- src/main/java/com/roxannnna/learning/variable/Array.java
+++ src/main/java/com/roxannnna/learning/variable/Array.java
@@ -15,5 +15,17 @@ public class Array {
         System.out.println("Element at index 2: " + anArray[2]);
         System.out.println("Element at index 3: " + anArray[3]);
         System.out.println("Element at index 4: " + anArray[4]);
+
+        //Dodawanie liczb
+
+        int num0 = anArray[0];
+        int num1 = anArray[1];
+        int num2 = anArray[2];
+        int num3 = anArray[3];
+        int num4 = anArray[4];
+
+        int total = num0 + num1 + num2 + num3 + num4;
+
+        System.out.println("Result: " + total);
     }
 }
