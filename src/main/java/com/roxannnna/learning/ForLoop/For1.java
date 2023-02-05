9a34353f136ba34ac4cde4deebd0c51e5999054e
Roxannnna <roksana2107@gmail.com>
2023-02-05 21:50:52 +0100

    learning

---
+++ src/main/java/com/roxannnna/learning/ForLoop/For1.java
@@ -0,0 +1,9 @@
+package com.roxannnna.learning.ForLoop;
+
+public class For1 {
+    public static void main(String[] args) {
+        for(int j = 9; j>1 && j<=15; j++) {
+            System.out.println("Count is: " + j);
+        }
+    }
+}
