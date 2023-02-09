cebc7e62885c0e6b5241b7308fd84a576f1909c2
Roxannnna <roksana2107@gmail.com>
2023-02-09 15:43:52 +0100

    learning

--- src/main/java/com/roxannnna/learning/Examples/CoinToss.java
+++ src/main/java/com/roxannnna/learning/Examples/CoinToss.java
@@ -4,43 +4,42 @@ import java.util.concurrent.ThreadLocalRandom;
 public class CoinToss {
     public static void main(String[] args) {
 
-            String o;
-            String orzel = o;
-            String r;
-            String reszka = r;
-            boolean guessed = false;
 
-            System.out.println("Program rzuca monetą: ");
-            String randomChoice = getRandomCoinHeadOrTail(orzel, reszka);
+        String orzel = "O";
+        String reszka = "R";
+        boolean guessed = false;
 
-            System.out.println("Zgadnij co wylosował program orzeł (o) czy reszka (r)?: ");
+        System.out.println("Program rzuca monetą: ");
+        String randomChoice = getRandomCoinHeadOrTail();
 
-            while (guessed == false) {
-                String userChar = getRandomChoiceFromUser();
-                if (userChar == randomChoice) {
-                    System.out.println("Brawo, zgadleś!");
-                    guessed = true;
-                } else {
-                    System.out.println("Twój wybór jest nieprawidłowy!");
+        System.out.println("Zgadnij co wylosował program orzeł (O) czy reszka (R)?: ");
 
-                }
+        while (guessed == false) {
+            String userChar = getRandomChoiceFromUser();
+            if (userChar.equals(randomChoice)) {
+                System.out.println("Brawo, zgadleś!");
+                guessed = true;
+            } else {
+                System.out.println("Twój wybór jest nieprawidłowy!");
 
-                static String getRandomChoiceFromUser() {
-                    System.out.println("Podaj znak orzeł (o) lub reszka (r): ");
-                    Scanner in = new Scanner(System.in);
-                    String enteredChar = String.valueOf(in.nextInt());
-                }
+            }
+        }
+    }
 
+    static String getRandomChoiceFromUser() {
+        System.out.println("Podaj znak orzeł (O) lub reszka (R): ");
+        Scanner in = new Scanner(System.in);
+        String enteredChar = String.valueOf(in.next());
+        return enteredChar;
     }
 
     //To metoda losujaca orla ("O") lub reszke ("R").
     static String getRandomCoinHeadOrTail() {
-        int randomNum = ThreadLocalRandom.current().nextInt(1,10) % 2;
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 10) % 2;
         if (randomNum == 0) {
             return "R";
         } else {
             return "O";
         }
     }
-}
 }
